da656861ba4313cb703fa96e2e1b7027e6ac0466
RafaelEmery <rafael.emerycade@gmail.com>
2020-05-17 15:31:33 -0300

    Comentarios adicionados - falta para a  all()

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -9,55 +9,60 @@ use App\Repositories\OrderRepository;
 use PDF;
 
 class OrderController extends Controller
-{
+{   
+    /**
+     * @return orders (Collection) de acordo com as buscas/$query feitas
+     */
+
     public function index()
     {      
+        /**
+         * Fazendo um filtro com Scopes:
+         * 
+         * - Usamos o metodo 'where' com uma funcao anonima dentro passando a $query ("busca")
+         * - Chamamos a scopeStatus que existem em Order.php
+         * - Deve ter o ->get() no final para termos uma Collection
+         * 
+         * Alternativas:
+         * 
+         * - Usar os scopes especificos em Order.php
+         * $orders = Order::delivered()->get();
+         * $orders = Order::pending()->get();
+         * 
+         * - Ou fazer um filtro "manual" usando 'switch($request->get('status'))' e 'case 'delivered': $orders = $orders->where('status','delivered');' 
+         * - OBS: O jeito acima consiste em atribuir Order::all() e ir "sobrescrevendo", e o condicional nao precisa ser por 'switch case'
+         */
+
         $orders = Order::where(function($query) {
-            
+
+            /**
+             * Usando a scopeStatus: 
+             * 
+             * - Puxando o campo 'status' para a query, conseguimos fazer um filtro de acordo com o status passado como parametro 
+             * - Em Order.php ao inves de usarmos scopes especificos como scopePending ou scopeDelivered, usamos o scopeStatus e passamos o status (valor do enum.) como parametro.
+             * 
+             * Alternativa:
+             * 
+             * - Usando scopes específicas (scopePending e scopeDelivered):
+             * - O filtro tambem pode ser feito usando os scopes especificos
+             * - Fazendo um condicional 'if(request()->get('status) == 'pending')' e dentro usamos a scope '$query->pending()'
+             * - Igual ao uso do scopePaid abaixo...
+             */
+
             if (request()->get('status')) {
                 $query->status(request()->get('status'));
             }
 
-            /*
-            if (request()->get('status') == 'pending') {
-                $query->pending();
-            }
-            if (request()->get('status') == 'delivered') {
-                $query->delivered();
-            }
-            */
-            
+            /**
+             * - Scope especifico para quando o campo 'paid' for verdadeiro 
+             * - Em Order.php usamos o scopePaid
+             */
+
             if (request()->get('paid') == 1) {
                 $query->paid();
             }
             
         })->get();
-        
-        /*
-        $orders = Order::delivered()->get();
-        $orders = Order::pending()->get();
-        */
-
-        /*
-        $orders = Order::all();
-
-        if (request()->get('status')) {
-            switch(request()->get('status')) {
-
-                case 'delivered':
-                    $orders = $orders->where('status', 'delivered');
-                break;
-
-                case 'pending':
-                    $orders = $orders->where('status', 'pending');
-                break;
-            }
-        }
-
-        if (request()->get('paid') == 1) {
-            $orders = $orders->where('paid', 1);
-        }
-        */
 
         return view('dashboard.index', compact('orders'));
     }
@@ -66,6 +71,11 @@ class OrderController extends Controller
     {
         return view('dashboard.create');
     }
+
+    /**
+     * @param OrderRequest como paramatro onde sao feitas todas as validacoes relacionada a este objeto que ira ser criado
+     * @return route 'orders.index' que leva a tabela principal
+     */
 
     public function store(OrderRequest $request) 
     {   
@@ -78,6 +88,15 @@ class OrderController extends Controller
     {
         return $repository->listAll();
     }
+
+    /**
+     * Gerando PDF da tabela 'orders':
+     * 
+     * - Foi usado o pacote barryvdh/laravel-dompdf
+     * - Baixado e instalado pelo composer e configurado em 'app.php'
+     * - O metodo loadview recebe como parametro uma view na pasta 'pdf' e uma Collection de orders
+     * @return pdf->download para um arquivo chamado 'orders_report.pdf'
+     */
 
     public function export()
     {
